6a11d416fcf6286526cdbb213f1f61418eddd370
Md. Reyanus Salehin <salehin24.rs@gmail.com>
2021-07-14 02:02:31 +0600

    dashboard added

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,5 +1,5 @@
 const getDashboard = (req, res) => {
-    res.render("users/dashboard.ejs", { req: req });
+    res.render("users/dashboard.ejs", { req: req, user:req.user });
 }
 
 module.exports = getDashboard;
