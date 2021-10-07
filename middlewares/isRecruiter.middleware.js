9dcf97e34bf66bd7f938cff12c4e77a4c51cb56f
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2021-10-07 23:23:24 +0600

    Revert "Reyan"

--- middlewares/isRecruiter.middleware.js
+++
@@ -1,9 +0,0 @@
-const isRecruiter = (req, res, next) => {
-    if (req.user.usertype == 'recruiter') {
-      next();
-    } else {
-      req.flash("error", "You do not have access!");
-      res.redirect("/");
-    }
-  };
-  module.exports = isRecruiter;
